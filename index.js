a3f8f655a93d36b2de6d27163884da51cefda52a
Vincent Peybernes [Techniv] <vpeybernes.pro@gmail.com>
2012-11-02 18:17:32 +0100

    Change license to MIT

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 /*
- * This file is part of Node CMD-Conf.
+ * This file is part of CMD-Conf.
  * 
- * Node-CMD-Conf is distributed under the terms of the 
- * GNU General Public License version 3.0.
+ * CMD-Conf is distributed under the terms of the M.I.T License.
  * 
- * Node-CMD-Conf is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * Copyright © Vincent Peybernes
  *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in 
+ * the Software without restriction, including without limitation the rights 
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do 
+ * so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all 
+ * copies or substantial portions of the Software.
+ *
+ * The Software is provided "as is", without warranty of any kind, express or 
+ * implied, including but not limited to the warranties of merchantability, fitness
+ * for a particular purpose and noninfringement. In no event shall the authors or 
+ * copyright holders be liable for any claim, damages or other liability, whether
+ * in an action of contract, tort or otherwise, arising from, out of or in 
+ * connection with the software or the use or other dealings in the Software.
  */
 
 /**
